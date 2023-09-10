5a9364aad395c4b37077b9a20e6aa14fbc873841
Aneesh Gidda <aneeshgidda@gmail.com>
2023-09-10 14:03:27 -0400

    Update loss_functions.py

--- object_detection_model/loss_functions.py
+++ object_detection_model/loss_functions.py
@@ -1,7 +1,18 @@
 import tensorflow as tf
 import numpy as np
 
+# Function to calculate the score loss
 def calc_score_loss(pred_scores, real_scores):
+    """
+    Calculate the score loss using cross-entropy loss.
+
+    Args:
+        pred_scores (list): Predicted scores for each anchor.
+        real_scores (list): Real scores for each anchor.
+
+    Returns:
+        Tensor: Score loss.
+    """
     score_loss = 0
     for real_score, pred_score in zip(real_scores, pred_scores):
         pred_probs = tf.nn.softmax(pred_score)
@@ -10,7 +21,18 @@ def calc_score_loss(pred_scores, real_scores):
     score_loss = score_loss / len(real_scores)
     return score_loss
 
+# Function to calculate the offset loss
 def calc_offset_loss(pred_offsets, real_offsets):
+    """
+    Calculate the offset loss using smooth L1 loss.
+
+    Args:
+        pred_offsets (Tensor): Predicted bounding box offsets.
+        real_offsets (Tensor): Real bounding box offsets.
+
+    Returns:
+        Tensor: Offset loss.
+    """
     delta_x = pred_offsets[:, 0] - real_offsets[:, 0]
     delta_y = pred_offsets[:, 1] - real_offsets[:, 1]
     delta_w = pred_offsets[:, 2] - real_offsets[:, 2]
@@ -23,7 +45,18 @@ def calc_offset_loss(pred_offsets, real_offsets):
     offset_loss = tf.reduce_sum(smooth_l1_x + smooth_l1_y + smooth_l1_w + smooth_l1_h)
     return offset_loss
 
+# Function to calculate the classifier loss
 def calc_classifier_loss(all_classes, all_ground_truth_classes):
+    """
+    Calculate the classifier loss using categorical cross-entropy loss.
+
+    Args:
+        all_classes (list): Predicted classes for each anchor.
+        all_ground_truth_classes (list): Ground truth classes for each anchor.
+
+    Returns:
+        Tensor: Classifier loss.
+    """
     classifier_loss = 0
     for classes, ground_truth_classes in zip(all_classes, all_ground_truth_classes):
         loss_object = tf.keras.losses.CategoricalCrossentropy()

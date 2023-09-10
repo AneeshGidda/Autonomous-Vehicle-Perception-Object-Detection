8f74bb951c8caa7a78682df9c637c7384246c93d
Aneesh Gidda <aneeshgidda@gmail.com>
2023-09-10 14:07:31 -0400

    Update region_proposal_network.py

--- object_detection_model/region_proposal_network.py
+++ object_detection_model/region_proposal_network.py
@@ -7,41 +7,58 @@ from convert import convert_proposals
 from loss_functions import calc_offset_loss, calc_score_loss
 from proposals import generate_proposals, filter_proposals
 
+# Define a Region Proposal Network (RPN) model
 class RegionProposalNetwork(tf.keras.Model):
     def __init__(self):
         super().__init__()
         self.num_of_anchors = 9
+        
+        # 1st Convolutional layer for feature extraction
         self.conv1 = tf.keras.layers.Conv2D(filters=2048, kernel_size=(3,3), padding="same", activation="relu", trainable=True)
+        
+        # Classifier layer for anchor classification
         self.classifier = tf.keras.layers.Conv2D(filters=self.num_of_anchors, kernel_size=(1,1), padding="same", activation="sigmoid", trainable=True)
+        
+        # Regressor layer for anchor regression
         self.regressor = tf.keras.layers.Conv2D(filters=4 * self.num_of_anchors, kernel_size=(1,1), padding="same", activation="linear", trainable=True)
 
     def call(self, feature_map, ground_truth_boxes=None):
+        # Determine the mode (training or evaluation)
         if ground_truth_boxes is None:
             mode = "eval"
         else:
             mode = "train"
 
+        # Extract features from the input feature map
         x = self.conv1(feature_map)
+        
+        # Generate anchor scores and offsets
         scores = self.classifier(x)
         offsets = self.regressor(x)
 
         if mode == "eval":
+            # In evaluation mode, generate anchors and evaluate them
             anchors, pred_scores, pred_offsets = anchors_eval(feature_map, scores, offsets)
 
         if mode == "train":
+            # In training mode, generate anchors, calculate losses, and filter proposals
             anchors, pred_scores, pred_offsets, real_scores, real_offsets = anchor_train(feature_map, scores, offsets, ground_truth_boxes)
             classifier_loss = calc_score_loss(pred_scores, real_scores)
             offset_loss = calc_offset_loss(pred_offsets, real_offsets)
             total_loss = classifier_loss + offset_loss
 
+        # Generate proposals based on anchors and predicted offsets
         proposals = generate_proposals(anchors, pred_offsets)
         num_ground_truth_boxes = np.shape(ground_truth_boxes)[1]
         num_proposals = max(num_ground_truth_boxes, np.shape(proposals)[1])
+        
+        # Filter proposals to keep the top ones using NMS
         proposals, scores = filter_proposals(proposals, pred_scores, number_of_proposals=num_proposals)
         nms_proposals = convert_proposals(proposals)
 
         final_proposals = []
         for proposals, scores in zip(nms_proposals, pred_scores):
+            # Apply Non-Maximum Suppression (NMS) to further filter proposals
             indices = tf.image.non_max_suppression(boxes=proposals, max_output_size=num_ground_truth_boxes, scores=scores, iou_threshold=0.7)
             proposals = tf.gather(proposals, indices=indices)
             final_proposals.append(proposals)
@@ -51,77 +68,3 @@ class RegionProposalNetwork(tf.keras.Model):
         
         if mode == "train":
             return final_proposals, total_loss
-    
-# feature_map = np.random.rand(1, 20, 20, 2048)
-# num_boxes = 7
-# ground_truth_boxes = np.random.randint(0, 20, size=(num_boxes, 4))
-# ground_truth_boxes[:, 2:] += ground_truth_boxes[:, :2]
-# ground_truth_boxes = np.reshape(ground_truth_boxes, newshape=(1, num_boxes, 4)).astype("float32")
-# print(np.shape(ground_truth_boxes))
-# model = RegionProposalNetwork()
-# optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
-
-# for epoch in range(10):
-#     with tf.GradientTape() as tape:
-#         loss = model(feature_map, ground_truth_boxes)
-
-#     # Print the trainable variables and their shapes
-#     trainable_variables = model.trainable_variables
-#     print("Trainable Variables:")
-#     for var in trainable_variables:
-#         print("Variable Name:", var.name)
-#         print("Variable Shape:", var.shape)
-
-#     # Calculate gradients
-#     grads = tape.gradient(loss, trainable_variables)
-#     print("Gradients:")
-#     optimizer.apply_gradients(zip(grads, model.trainable_weights))
-#     avg_loss = tf.reduce_mean(loss)
-#     print(f"Epoch [{epoch + 1}/{10}] Loss: {avg_loss.result().numpy():.4f}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# if mode == "eval":
-#             scores = np.reshape(scores, newshape=(batch, -1))
-#             box_deltas = np.reshape(box_deltas, newshape=(batch, -1, 4))
-#             final_proposals = []
-
-#             for box_deltas, scores in zip(box_deltas, scores):
-#                 anchor_boxes, valid_map = anchors(feature_map)
-#                 scores = scores[valid_map]
-#                 box_deltas = box_deltas[valid_map]
-
-#                 number_of_anchors = np.shape(anchor_boxes)[0]
-#                 objectness_score = np.full(number_of_anchors, 0)
-#                 objectness_score[scores >= 0.7] = 1
-
-#                 positive_anchors_index = np.where(objectness_score == 1)
-#                 positive_anchors_index = np.squeeze(positive_anchors_index)
-#                 positive_anchors = anchor_boxes[positive_anchors_index]
-#                 box_deltas = box_deltas[positive_anchors_index, :]
-
-#                 image_space_positive_anchors = convert_boxes(positive_anchors, [375, 1242, 3], feature_map, mode="feature_to_image")
-#                 image_space_positive_anchors = tf.cast(image_space_positive_anchors, dtype="float64")
-#                 box_deltas = tf.cast(box_deltas, dtype="float64")
-#                 proposals = generate_proposals(image_space_positive_anchors, box_deltas)
-#                 final_proposals.append(proposals) 
-#             proposals = np.squeeze(np.array(final_proposals))

e6870dc23f254d7dd40b48cfe99c9d5aaed8cc40
Aneesh Gidda <aneeshgidda@gmail.com>
2023-09-10 14:10:23 -0400

    Update roi_align.py

--- object_detection_model/roi_align.py
+++ object_detection_model/roi_align.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 def create_roi_grid(region_of_interest, pooling_height, pooling_width):
+    """
+    Create a grid of regions of interest (ROIs) for ROIAlign.
+
+    Args:
+        region_of_interest (list): A list [x_min, y_min, x_max, y_max] defining the ROI.
+        pooling_height (int): Height of the ROI grid.
+        pooling_width (int): Width of the ROI grid.
+
+    Returns:
+        np.ndarray: An array representing the ROI grid.
+    """
     x_min, y_min, x_max, y_max = region_of_interest
     grid_width = x_max - x_min
     grid_height = y_max - y_min
@@ -19,26 +30,18 @@ def create_roi_grid(region_of_interest, pooling_height, pooling_width):
             roi_grid[row, col] = square
     return roi_grid
 
-# def generate_sample_points(region_of_interest, pooling_height, pooling_width):
-#     x_min, y_min, x_max, y_max = region_of_interest
-#     width = x_max - x_min
-#     height = y_max - y_min
-
-
-#     grid_height = height / pooling_height
-#     grid_width = width / pooling_width
-#     rounded_grid_height = np.ceil(height / pooling_height)
-#     rounded_grid_width = np.ceil(width / pooling_width)
-#     sampling_points = []
-
-#     for iy in range(int(rounded_grid_height)):
-#         y_coordinate = y_min + rounded_grid_height + (iy + 0.5) * rounded_grid_height / grid_height
-#         for ix in range(int(rounded_grid_width)):
-#             x_coordinate = x_min + rounded_grid_width + (ix + 0.5) * rounded_grid_width / grid_width
-#             sampling_points.append([x_coordinate, y_coordinate])
-#     return np.array(sampling_points)
-
 def generate_sample_points(region_of_interest, horizontal_sampling_points=2, vertical_sampling_points=2):
+    """
+    Generate sampling points within an ROI.
+
+    Args:
+        region_of_interest (list): A list [x_min, y_min, x_max, y_max] defining the ROI.
+        horizontal_sampling_points (int): Number of horizontal sampling points.
+        vertical_sampling_points (int): Number of vertical sampling points.
+
+    Returns:
+        np.ndarray: An array representing the sampling points.
+    """
     x_min, y_min, x_max, y_max = region_of_interest
     width = x_max - x_min
     height = y_max - y_min
@@ -51,37 +54,18 @@ def generate_sample_points(region_of_interest, horizontal_sampling_points=2, ver
             sampling_points.append([x_coordinate, y_coordinate])
     return np.array(sampling_points)
 
-# def bilinear_interpolation(feature_map, sampling_points):
-#     height = np.shape(feature_map)[1]
-#     width = np.shape(feature_map)[2]
-
-#     number_of_sampling_points = np.shape(sampling_points)[0]
-#     number_of_channels = np.shape(feature_map)[3]
-#     output = np.zeros(shape=(number_of_sampling_points, number_of_channels))
-
-#     for i, (x, y) in enumerate(sampling_points):
-#         y_low = int(y); x_low = int(x)
-#         if (y_low >= height - 1):
-#             y_high = y_low = height - 1
-#             y = y_low
-#         else:
-#             y_high = y_low + 1
-
-#         if (x_low >= width-1):
-#             x_high = x_low = width-1
-#             x = x_low
-#         else:
-#             x_high = x_low + 1
-
-#         ly = y - y_low; lx = x - x_low
-#         hy = 1 - ly; hx = 1 - lx
-#         w1 = hy * hx; w2 = hy * lx; w3 = ly * hx; w4 = ly * lx
-#         interpolated_value = w1 * feature_map[:, y_low, x_low, :] + w2 * feature_map[:, y_low, x_high, :] + \
-#                              w3 * feature_map[:, y_high, x_low, :] +  w4 * feature_map[:, y_high, x_high, :]
-#         output[i, :] = interpolated_value
-#     return output
-
 def bilinear_interpolation(feature_map, sampling_points, image_num):
+    """
+    Perform bilinear interpolation to sample values from the feature map.
+
+    Args:
+        feature_map (np.ndarray): The input feature map.
+        sampling_points (np.ndarray): An array of sampling points.
+        image_num (int): Index of the input image.
+
+    Returns:
+        np.ndarray: An array of interpolated values.
+    """
     height = np.shape(feature_map)[1]
     width = np.shape(feature_map)[2]
 
@@ -110,6 +94,18 @@ def bilinear_interpolation(feature_map, sampling_points, image_num):
     return output
 
 def roi_align(feature_map, all_proposals, pooling_height, pooling_width):
+    """
+    Perform ROIAlign operation on a feature map.
+
+    Args:
+        feature_map (np.ndarray): The input feature map.
+        all_proposals (np.ndarray): An array of ROIs for all images in the batch.
+        pooling_height (int): Height of the ROI grid.
+        pooling_width (int): Width of the ROI grid.
+
+    Returns:
+        np.ndarray: An array of pooled ROI features.
+    """
     batch = np.shape(all_proposals)[0]
     for image_num, proposals in enumerate(all_proposals):
         number_of_roi = np.shape(proposals)[0]
@@ -125,18 +121,3 @@ def roi_align(feature_map, all_proposals, pooling_height, pooling_width):
                     sampling_point_values = bilinear_interpolation(feature_map, sampling_points, image_num)
                     output[image_num, roi_number, row_number, col_number, :] = np.sum(sampling_point_values, axis=0) / number_of_sampling_points
         return output
-
-# feature_map = np.random.rand(1, 94, 311, 2048)
-# proposals = np.random.rand(1, 10, 4)
-# pooling_height = 7
-# pooling_width = 7
-
-# roi_grid = create_roi_grid([0, 0, 7, 7], pooling_height, pooling_width)
-# print(roi_grid)
-# for row_number, row in enumerate(roi_grid):
-#     for col_number, col in enumerate(row):
-#         sampling_points = generate_sample_points(col, pooling_height, pooling_width)
-#         print(np.shape(sampling_points))
-
-# output = roi_align(feature_map, proposals, pooling_height, pooling_width)
-# print(np.shape(output))

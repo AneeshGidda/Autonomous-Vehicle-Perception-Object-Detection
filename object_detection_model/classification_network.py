2b91b00562b756ebf7c8d10e4f126ef9929b6792
Aneesh Gidda <aneeshgidda@gmail.com>
2023-09-10 13:55:54 -0400

    Update classification_network.py

--- object_detection_model/classification_network.py
+++ object_detection_model/classification_network.py
@@ -2,78 +2,65 @@ import tensorflow as tf
 from roi_align import roi_align
 from loss_functions import calc_classifier_loss
 
+# Define a ClassificationNetwork class that extends tf.keras.Model
 class ClassificationNetwork(tf.keras.Model):
     def __init__(self, num_classes):
         super().__init__()
+
+        # Reshape layer to match the expected input shape
         self.reshape = tf.keras.layers.Reshape(target_shape=(22, -1))
+        
+        # First fully connected layer with 128 units and ReLU activation
         self.fully_connected_layer1 = tf.keras.layers.Dense(units=128, activation="relu")
+        
+        # Dropout layer to prevent overfitting
         self.dropout1 = tf.keras.layers.Dropout(0.3)
+        
+        # Second fully connected layer with 64 units and ReLU activation
         self.fully_connected_layer2 = tf.keras.layers.Dense(units=64, activation="relu")
+        
+        # Another dropout layer
         self.dropout2 = tf.keras.layers.Dropout(0.3)
+        
+        # Classifier layer with 'num_classes' units and softmax activation
         self.classifier = tf.keras.layers.Dense(units=num_classes, activation="softmax")
 
     def call(self, feature_map, proposals, ground_truth_classes=None):
+        # Determine the mode: 'eval' or 'train'
         if ground_truth_classes is None:
             mode = "eval"
         else:
             mode = "train"
 
+        # Perform Region of Interest (ROI) pooling using the provided feature map and proposals
         x = roi_align(feature_map=feature_map, 
                       all_proposals=proposals,
                       pooling_height=4,
                       pooling_width=4)
 
+        # Reshape the pooled features
         x = self.reshape(x)
+
+        # Pass the features through the first fully connected layer and apply ReLU activation
         x = self.fully_connected_layer1(x)
+        
+        # Apply dropout to the first layer's output
         x = self.dropout1(x)
+        
+        # Pass the features through the second fully connected layer and apply ReLU activation
         x = self.fully_connected_layer2(x)
+        
+        # Apply dropout to the second layer's output
         x = self.dropout2(x)
+        
+        # Generate class probabilities using the classifier layer
         classes = self.classifier(x)
 
+        # In evaluation mode, return the predicted classes
         if mode == "eval":
             return classes
         
+        # In training mode, calculate the classifier loss
         if mode == "train":
             classifier_loss = calc_classifier_loss(classes, ground_truth_classes)
             return classifier_loss
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-# # Sample feature map of shape [1, 94, 311, 2048]
-# sample_feature_map = np.random.randn(1, 94, 311, 2048).astype(np.float32)
-
-# # Sample proposals (format: [x1, y1, x2, y2])
-# sample_proposals = np.array([
-#     [100, 100, 200, 200],
-#     [150, 150, 250, 250],
-#     [200, 200, 300, 300],
-#     # ... add more sample proposals
-# ])
-
-# # Create an instance of the DetectorNetwork model
-# num_classes = 3  # Change this to the number of classes in your problem
-# model = DetectorNetwork(num_classes)
-
-# # Convert the sample feature map and proposals to TensorFlow tensors
-# tf_feature_map = tf.convert_to_tensor(sample_feature_map)
-# tf_proposals = tf.convert_to_tensor(sample_proposals, dtype=tf.float32)
-
-# # Call the model with the sample inputs
-# sample_classes = model(tf_feature_map, tf_proposals)
-
-# print("Sample Classes:", sample_classes)
-# print("Sample Classes Shape:", sample_classes.shape)
